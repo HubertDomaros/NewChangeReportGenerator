01f323ce465113574a6a6eadd2122f74c7a5f9e3
hubdomaros@gmail.com <hubdomaros@gmail.com>
2022-03-08 14:35:54 +0100

    Added method for setting values of public parameters and applied that method in constructor

--- NewChangeReportGenerator/Core/SortedData.cs
+++ NewChangeReportGenerator/Core/SortedData.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections.Generic;
+using DocumentFormat.OpenXml.Packaging;
 using NewChangeReportGenerator.OpenXMLProcessor.ExcelProcessor;
 
 namespace NewChangeReportGenerator.Core;
 
 public class SortedData {
-
-    private ExcelDocumentParser _excelDocumentParser;
-    public List<string> RowNumberArray { get; private set; }
-    public List<string> SapObjectArray { get; private set; }
+    private WorkbookPart _workbookPart;
+    
+    public List<string> RowNumberList { get; private set; }
+    public List<string> SapObjectList { get; private set; }
+    public List<string> DefinedByList { get; private set; }
     public Dictionary<string, string>[] DefinedByDictionariesArray { get; private set; }
 
-    public SortedData(ExcelDocumentParser excelDocumentParser) {
-        _excelDocumentParser = excelDocumentParser;
+    private void SetClassProprieties() {
+        ExcelColumnParser columnParser = new ExcelColumnParser(_workbookPart);
+        MainSortingAlgorithm mainSortingAlgorithm =
+            new MainSortingAlgorithm(RowNumberList, SapObjectList, DefinedByList);
+
+        RowNumberList = columnParser.UrlColumnToStringList("A");
+        SapObjectList = columnParser.TextColumnToStringList("B");
+        DefinedByList = columnParser.TextColumnToStringList("C");
+        DefinedByDictionariesArray = mainSortingAlgorithm.GetDefinedByItemsWithUrls();
+    }
+
+    public SortedData(WorkbookPart workbookPart) {
+        _workbookPart = workbookPart;
+        SetClassProprieties();
     }
 }
