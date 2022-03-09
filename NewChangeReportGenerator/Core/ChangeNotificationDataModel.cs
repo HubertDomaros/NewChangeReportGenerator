82c7b11a612ead212d96107993d0b7f8bb160f26
hubdomaros@gmail.com <hubdomaros@gmail.com>
2022-03-09 18:58:36 +0100

    Changed class name; adusted parameters to classes used in methods

--- NewChangeReportGenerator/Core/ChangeNotificationDataModel.cs
+++ NewChangeReportGenerator/Core/ChangeNotificationDataModel.cs
@@ -6,45 +6,34 @@ using DocumentFormat.OpenXml.Packaging;
 
 namespace ChangeNotificationGenerator.Core;
 
-public class ChangeNotificationDataService {
+public class ChangeNotificationDataModel {
     private readonly WorkbookPart _workbookPart;
     
     public List<string> RowNumberList { get; private set; }
     public List<string> SapObjectList { get; private set; }
     public List<string> DefinedByList { get; private set; }
-    public Dictionary<string, string>[] DefinedByDictionariesArray { get; private set; }
+    public List<Dictionary<string, string>> DefinedByItemsWithUrls { get; private set; }
 
     private void SetClassProprieties() {
         ExcelColumnParser columnParser = new ExcelColumnParser(_workbookPart);
-        
-
         
         RowNumberList = columnParser.UrlColumnToStringList("A");
         SapObjectList = columnParser.TextColumnToStringList("B");
         DefinedByList = columnParser.TextColumnToStringList("C");
 
-        PrintDebugProperties();
-
         MainSortingAlgorithm mainSortingAlgorithm =
             new MainSortingAlgorithm(RowNumberList, SapObjectList, DefinedByList);
-        DefinedByDictionariesArray = mainSortingAlgorithm.GetDefinedByItemsWithUrls();
-    }
-
-    private void PrintListWhileDebugging(List<string> debuggedList) {
-        foreach (var item in debuggedList) {
-            Debug.Print(item);
-        }
-        Debug.Print("");
+        DefinedByItemsWithUrls = mainSortingAlgorithm.DefinedByItemsWithUrls();
     }
 
     private void PrintDebugProperties() {
         DebugUtils.PrintDebuggedList(RowNumberList, "Printing RowNumberList");
         DebugUtils.PrintDebuggedList(SapObjectList, "Printing SapObjectList");
         DebugUtils.PrintDebuggedList(DefinedByList, "Printing DefinedByList");
-        DebugUtils.PrintDebuggedDictionariesArray(DefinedByDictionariesArray, "Printing DefinedByDictionariesArray");
+        DebugUtils.PrintDebuggedDictionariesList(DefinedByItemsWithUrls, "Printing DefinedByItemsWithUrls");
     }
 
-    public ChangeNotificationDataService(WorkbookPart workbookPart) {
+    public ChangeNotificationDataModel(WorkbookPart workbookPart) {
         _workbookPart = workbookPart;
         SetClassProprieties();
 #if DEBUG
