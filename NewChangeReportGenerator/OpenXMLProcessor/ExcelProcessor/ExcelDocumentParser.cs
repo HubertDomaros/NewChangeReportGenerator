e6a3de83c107c9b3c6326d01c95c587dbb229157
hubdomaros@gmail.com <hubdomaros@gmail.com>
2022-03-09 19:04:50 +0100

    Class rewritten due to changes in class structure

--- NewChangeReportGenerator/OpenXMLProcessor/ExcelProcessor/ExcelDocumentParser.cs
+++ NewChangeReportGenerator/OpenXMLProcessor/ExcelProcessor/ExcelDocumentParser.cs
@@ -1,26 +1,22 @@
 ﻿using System.Diagnostics;
+using ChangeNotificationGenerator.Core;
 using DocumentFormat.OpenXml.Packaging;
 
 namespace ChangeNotificationGenerator.OpenXMLProcessor.ExcelProcessor; 
 
 public class ExcelDocumentParser {
     private readonly string _filePath;
-    private readonly SpreadsheetDocument _spreadsheetDocument;
-    public WorkbookPart CurrentWorkbookPart { get; private set; }
+    public ChangeNotificationDataModel ChangeNotificationDataModel { get; }
 
+    private ChangeNotificationDataModel ProcessChangeNotificationData() {
+        using var spreadsheetDocument = SpreadsheetDocument.Open(_filePath, false);
+        var dataModel = new ChangeNotificationDataModel(spreadsheetDocument.WorkbookPart);
 
+        return dataModel;
+    }
 
     public ExcelDocumentParser(string filePath) {
         _filePath = filePath;
-        var spreadsheetDocument = SpreadsheetDocument.Open(_filePath, false);
-        _spreadsheetDocument = spreadsheetDocument;
-        if (spreadsheetDocument.WorkbookPart != null) {
-            CurrentWorkbookPart = spreadsheetDocument.WorkbookPart;
-        } else Debug.Print("WorkbookPart is null");
-    }
-
-     ~ExcelDocumentParser() {
-        _spreadsheetDocument.Close();
-        Debug.Print("Excel file closed successfully");
+        ChangeNotificationDataModel = ProcessChangeNotificationData();
     }
 }
