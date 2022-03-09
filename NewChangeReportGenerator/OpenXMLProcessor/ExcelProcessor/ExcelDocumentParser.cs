3c4de9aab23a262b8e14266ee887342afc7b2347
hubdomaros@gmail.com <hubdomaros@gmail.com>
2022-03-09 10:56:08 +0100

    Added experimental constructor and destructor

--- NewChangeReportGenerator/OpenXMLProcessor/ExcelProcessor/ExcelDocumentParser.cs
+++ NewChangeReportGenerator/OpenXMLProcessor/ExcelProcessor/ExcelDocumentParser.cs
@@ -1,5 +1,26 @@
-﻿namespace NewChangeReportGenerator.OpenXMLProcessor.ExcelProcessor; 
+﻿using System.Diagnostics;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace NewChangeReportGenerator.OpenXMLProcessor.ExcelProcessor; 
 
 public class ExcelDocumentParser {
-    
+    private readonly string _filePath;
+    private readonly SpreadsheetDocument _spreadsheetDocument;
+    public WorkbookPart CurrentWorkbookPart { get; private set; }
+
+
+
+    public ExcelDocumentParser(string filePath) {
+        _filePath = filePath;
+        var spreadsheetDocument = SpreadsheetDocument.Open(_filePath, false);
+        _spreadsheetDocument = spreadsheetDocument;
+        if (spreadsheetDocument.WorkbookPart != null) {
+            CurrentWorkbookPart = spreadsheetDocument.WorkbookPart;
+        } else Debug.Print("WorkbookPart is null");
+    }
+
+     ~ExcelDocumentParser() {
+        _spreadsheetDocument.Close();
+        Debug.Print("Excel file closed successfully");
+    }
 }
