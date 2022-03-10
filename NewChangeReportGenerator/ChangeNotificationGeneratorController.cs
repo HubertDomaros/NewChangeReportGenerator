495369ec750f9b283ba0e5fd07caa8992f087aa3
HubertDomaros <hubdomaros@gmail.com>
2022-03-11 00:22:54 +0100

    Moved processing of input data to ExcelDocumentParser

--- NewChangeReportGenerator/ChangeNotificationGeneratorController.cs
+++ NewChangeReportGenerator/ChangeNotificationGeneratorController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ChangeNotificationGenerator.Core;
+using ChangeNotificationGenerator.OpenXMLProcessor.ExcelProcessor;
 using ChangeNotificationGenerator.OpenXMLProcessor.WordProcessor;
 using DocumentFormat.OpenXml.Packaging;
 
@@ -11,12 +12,8 @@ public class ChangeNotificationGeneratorController {
     
 
     public void ProcessExcelDocument(string excelFilePath) {
-        using var spreadsheetDocument = SpreadsheetDocument.Open(excelFilePath, false);
-        var workbookPart = spreadsheetDocument.WorkbookPart;
-        if (workbookPart == null) throw new ArgumentNullException("Workbook is null or empty");
-
-        var changeNotificationDataModel = new ChangeNotificationDataModel(workbookPart);
-        _changeNotificationDataModel = changeNotificationDataModel;
+        var excelDocumentParser = new ExcelDocumentParser(excelFilePath);
+        _changeNotificationDataModel = excelDocumentParser.ProcessChangeNotificationData();
     }
 
     public void GenerateChangeNotificationDocument(string wordFilePath, CheckboxesConfig checkboxesConfig) {
