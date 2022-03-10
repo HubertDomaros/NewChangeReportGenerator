8fe4f8a37fe06d9b0c3cd7bd642c659aaf44bb40
HubertDomaros <hubdomaros@gmail.com>
2022-03-11 00:21:23 +0100

    Initial commit

---
+++ NewChangeReportGenerator/Warnings/ElementsNotFoundWarning.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+
+namespace ChangeNotificationGenerator.Warnings;
+public class ElementsNotFoundWarning {
+    private readonly List<string> _notFoundElementsList;
+
+    private void ShowMessageBox() {
+        var message = "Excel file opened with warnings! \n" +
+                         "Following items were not found in Object list: \n";
+
+        foreach (var str in _notFoundElementsList) {
+            message = message + str + "\n";
+        }
+
+        MessageBox.Show(message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+    }
+
+    public ElementsNotFoundWarning(List<string> notFoundElementsList) {
+        _notFoundElementsList = notFoundElementsList;
+        ShowMessageBox();
+    }
+}
